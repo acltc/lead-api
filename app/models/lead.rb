dc260ec12d5138c10b88b3da5430890d09db3d2c
Nikki Veen <nikkiveen@gmail.com>
2017-02-22 14:24:28 -0600

    added model methods to send text and email after lead is created

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,2 +1,21 @@
 class Lead < ApplicationRecord
+  after_create :send_text, :send_first_email
+
+  def send_text
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+    twilio_number = ENV['TWILIO_NUMBER']
+    text_to_number = phone
+
+    @client = Twilio::REST::Client.new account_sid, auth_token
+
+    message = @client.messages.create({ :from => twilio_number,
+                                        :to => text_to_number,
+                                        :body => "Hello from Actualize! Are you available to chat?"
+                                      })
+  end
+
+  def send_first_email
+    LeadContactMailer.first_message(self).deliver
+  end
 end
