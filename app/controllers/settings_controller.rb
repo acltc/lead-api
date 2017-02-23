e38df963d2b5254d6a9e91401e6ed6bd0e2095ee
Nikki Veen <nikkiveen@gmail.com>
2017-02-22 20:35:01 -0600

    edit settings -- email & phone

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,6 +2,24 @@ class SettingsController < ApplicationController
   before_action :authenticate_admin!
 
   def show
-    @settings = Setting.find(1)
+    @settings = Setting.first
+  end
+
+  def edit
+    @settings = Setting.find(params[:id])
+  end
+
+  def update
+    @settings = Setting.find(params[:id])
+    if @settings.update(
+      email: params[:settings]["email"],
+      phone: params[:settings]["phone"]
+    )
+      flash[:notice] = "Settings successfully updated."
+      redirect_to setting_path(@settings)
+    else
+      flash[:alert] = "An error occured while updating settings. Please try again."
+      render :edit
+    end
   end
 end
