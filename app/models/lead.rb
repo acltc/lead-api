224831ba58bdffaeb13b85e1867070f77210dc93
Nikki Veen <nikkiveen@gmail.com>
2017-02-27 11:53:38 -0600

    uncomment send text/email for demo

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,5 +1,5 @@
 class Lead < ApplicationRecord
-  # after_create :send_text, :send_first_email
+  after_create :send_text, :send_first_email
 
   def send_text
     account_sid = ENV['TWILIO_ACCOUNT_SID']
