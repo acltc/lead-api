d1aa53c678e2df28d437c748439ae7769b2d6436
Nikki Veen <nikkiveen@gmail.com>
2017-03-01 13:02:58 -0600

    updated mailer to ensure signature image shows up

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -2,6 +2,7 @@ class LeadMailer < ApplicationMailer
   default from: Setting.first.email
 
   def first_message(lead)
+    attachments.inline['actualize-logo.png'] = File.read('app/assets/images/actualize-logo.png')
     @lead = lead
     mail(to: @lead.email, subject: 'Hello from Actualize!')
   end
